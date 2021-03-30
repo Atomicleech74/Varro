3f52d8f470897f8cc9218fd0ebc12e8aeb5ef910
Atomicleech74 <nferreira828@g.rwu.edu>
2021-03-30 10:49:25 -0400

    camera working
    working camera displaying local video

--- app/src/main/java/com/example/varro/ui/call/CallFragment.java
+++ app/src/main/java/com/example/varro/ui/call/CallFragment.java
@@ -1,11 +1,13 @@
 package com.example.varro.ui.call;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -28,7 +30,17 @@ public class CallFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_call, container, false);
 
         WebView webView = (WebView)root.findViewById(R.id.web_view);
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                Log.println(Log.INFO, null, "REQUESTING PERMISSIONS==============================================");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
+
+        });
         webView.setBackgroundColor(Color.rgb(230,230,230));
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAllowFileAccess(true);
